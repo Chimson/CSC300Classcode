ef3e7d9936fdd5e5649acfda6113a206802f107a
Chimson <bensonharkison10@hotmail.com>
2026-09-14 12:30:33 -0500

    fixed spacing

--- eclipse_workspace/project/src/main/java/classcode/MoreJava.java
+++ eclipse_workspace/project/src/main/java/classcode/MoreJava.java
@@ -19,8 +19,9 @@ public class MoreJava {
   public static void reverseInPlace(double[] arr) {
     /*
     TODO: Complete reverseInPlace so that reverses the array by 
-          swaping values at indices 0 and N-1, 1 and N-2, etc. 
-     */
+          swaping values at indices 0 and N-1, 1 and N-2, etc.
+   */
+
      // {1, 2, 3, 4, 5}
      //   {5, 2, 3, 4, 1}
      //   {5, 4, 3, 2, 1}
@@ -46,8 +47,8 @@ public class MoreJava {
     // object types check equality differently
     // most objects are like this (except raw arrays and Strings)
     // this includes custom objects with .equals() overridden appropriately
-    ArrayList<Integer> lst1 = new ArrayList<Integer>();
-    ArrayList<Integer> lst2 = new ArrayList<Integer>();
+    ArrayList<Integer> lst1 = new ArrayList<>();
+    ArrayList<Integer> lst2 = new ArrayList<>();
     lst1.add(1); lst1.add(2);
     lst2.add(1); lst2.add(2);
     println(lst1);   
@@ -65,7 +66,7 @@ public class MoreJava {
     println(Arrays.toString(arr1));
     println(Arrays.toString(arr2));
     println(arr1 == arr2);   // .equals() is the same, not overridden
-                             //
+
     // TODO: use the arr1.equals(arr2) with raw arrays, but unimpl
 
     println(Arrays.equals(arr1, arr2)); 
@@ -87,9 +88,9 @@ public class MoreJava {
     StdOut.println(n1 == n2);
     StdOut.println(n1.equals(n2));
     printStars();
+
     // can create aliases on object types through variables
     // change an object using its alias
-    
     StdOut.println("aliases ...");
     double[] arrA = {1.1, 1.2, 1.3};
     double[] arrB = arrA;
@@ -101,10 +102,9 @@ public class MoreJava {
     println(arrA == arrB);   // aliases, hold the same reference, true
                                
     // TODO: add using Arrays.equals() to check values
-      
     println(arrACopy.equals(arrA));  // false, bc they are raw arrays, no .equals impl
     printStars();
-    
+
     // pass by value functions
     int val2 = 0;
     int[] arr4 = {99, 98, 97};
@@ -115,7 +115,6 @@ public class MoreJava {
    
     // int err = 1/0;
     // handle an exception
-    
     try {
       double badval = 1/0;
       println("Does not execute");
@@ -136,19 +135,18 @@ public class MoreJava {
     println("Continue!");
     
     // call static function
+    printStars();
     double[] darr = {1.0, 2.0, 3.0, 4.0};
     reverseInPlace(darr);
-    println(Arrays.toString(darr));
+    println("reversed results: " +  Arrays.toString(darr));
     printStars();
-    
-    
+
     // arrays will not allow access to elements in invalid indices, throws exception
     int[] iarr4 = new int[] {5, 6, 7, 8, 9, 10};
     for (int i = 0; i < iarr4.length - 1; ++i) {  // show the bounds check error too
       iarr4[i + 1] += iarr4[i];
     }
     StdOut.println(Arrays.toString(iarr4));
-    
     StdOut.println("end of file");
 
    
